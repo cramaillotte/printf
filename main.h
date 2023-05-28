ada2098440b7059bb173421c37f5d1c2af2bef8c
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-05-28 11:07:45 -0700

    _printf.c

--- main.h
+++ main.h
@@ -15,6 +15,9 @@ int _strlen(char *s);
 int print_char(char ch, int *counter);
 int print_percent(int *counter);
 int print_string(const char *str, int *counter);
+void process_char(char ch, int *counter);
+void process_string(const char *str, int *counter);
+void process_percent(int *counter);
 void print_unknown(const char *format, int *counter);
 
 /* main source code file _printf */
